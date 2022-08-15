d4884c3172998b1a1d8efdd848b64bf338b2eacd
Christopher Lam <calamity@chromium.org>
2022-08-15 19:45:04 +1000

    relink bar

---
+++ xmonad/newbar2/bar/extra/top.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xterm -geometry -7+24 -bg black -fg gray80 -fa Courier\ New -fs 14 -name top  -class top -e top
---
+++ xmonad/newbar2/bar/src/Utils.hs
@@ -0,0 +1,53 @@
+module Utils (
+  join,
+  strip,
+  fi,
+  fmtBytes,
+  perSec,
+  readKeyValueFile,
+  readFully
+  ) where
+
+import Text.Printf
+import qualified Data.Map as M
+import qualified Data.ByteString as Str
+import qualified Data.ByteString.Char8 as Char8
+
+join :: String -> [String] -> String
+join _ [] = ""
+join sep (x:xs) = foldl (\a b ->  a++sep++b ) x xs
+
+strip :: String -> String
+strip = reverse . dropWhile p . reverse . dropWhile p where
+  p = (==' ')
+
+split1 :: Eq t => t -> [t] -> ([t], [t])
+split1 ch s = (x, drop 1 xs) where
+  (x,xs) = break (==ch) s
+
+fi :: (Integral a, Num b) => a -> b
+fi = fromIntegral
+
+readFully :: FilePath -> IO String
+readFully f = Char8.unpack <$> Str.readFile f
+
+fmtBytes :: Int -> String
+fmtBytes b
+  | b < 1024 = printf "%d bytes" b
+  | b < (1024 * 1024) = printf "%d KiB" (b `div` 1024)
+  | b < (10 * 1024 * 1024) = printf "%.1f MB" (bf / (1024 * 1024))
+  | b >= (10 * 1024 * 1024) = printf "%d MB" (b `div` (1024 * 1024))
+  | otherwise = ""
+ where
+    bf = fromIntegral b :: Double
+
+readKeyValueFile :: (String -> a) -> FilePath -> IO (M.Map String a)
+readKeyValueFile pp filename = makeMap <$> readFully filename where
+  makeMap l = M.fromList $ map parseLine . lines $ l
+  parseLine l = (strip k, pp v) where
+     (k,v) = split1 ':' l
+
+perSec :: (Integral i) => Double -> i -> i
+perSec sec val = truncate $ fi val / sec
+
+
